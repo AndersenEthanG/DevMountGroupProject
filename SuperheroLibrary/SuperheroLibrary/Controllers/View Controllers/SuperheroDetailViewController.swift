9c26ea36dd72c1d67532bb0f132cf5c1b0a74adc
Ethan Andersen <AndersenEthanG@gmail.com>
2021-06-14 10:21:54 -0600

    Initial creation of DetailVC

---
+++ SuperheroLibrary/SuperheroLibrary/Controllers/View Controllers/SuperheroDetailViewController.swift
@@ -0,0 +1,41 @@
+//
+//  SuperheroDetailViewController.swift
+//  SuperheroLibrary
+//
+//  Created by Ethan Andersen on 6/14/21.
+//
+
+import UIKit
+
+class SuperheroDetailViewController: UIViewController {
+
+    // MARK: - Outlets
+    
+    
+    // MARK: - Properties
+    var hero: Hero?
+    
+
+    // MARK: - Lifecycle
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        updateView()
+    }
+
+    
+    // MARK: - Functions
+    func updateView() {
+        // Grab proper data from hero property
+        
+        // Set outlets
+    }
+    
+} // End of Class
+
+
+
+
+class Hero {
+    // Placeholder
+} // End of Class
